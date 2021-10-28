53f243a733dd4dffd59ddf89a80d8d0f5babb7b8
Yu4iy <yurimohyliuk@gmail.com>
2021-10-28 18:31:05 +0200

    add JSnack 5 block-2

---
+++ js-snacks-blocco-2/jsnack5/js/main.js
@@ -0,0 +1,33 @@
+
+// JSnack 5
+// Crea due array che hanno un numero di elementi diversi.
+// Aggiungi elementi casuali all’array che ha meno elementi,
+// fino a quando ne avrà tanti quanti l’altro.
+const array1 = [
+	'1',
+	'2',
+	'3',
+	'4',
+	'5',
+	'6',
+	'7',
+	'8'
+];
+const array2 = [
+	'1',
+	'2',
+	'3',
+	'4',
+	'5'
+];
+
+while(array1.length !== array2.length){
+	if(array1.length > array2.length){
+	array2.push('1')
+	}
+	else(
+	array.push('1')
+	)
+}
+console.log(array1)
+console.log(array2)
